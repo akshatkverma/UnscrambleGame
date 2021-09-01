caff2189590f71d6e3485f661835c42daf03ae8b
Akshat Kumar Verma <akshatvermajbp@gmail.com>
2021-09-01 15:50:49 +0530

    Removed the bug, if entered a string in first screen, game crashed

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameInitializer.kt
@@ -0,0 +1,47 @@
+package com.example.android.unscramble.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.android.unscramble.R
+import com.example.android.unscramble.databinding.GameInitializerBinding
+
+class GameInitializer: Fragment() {
+
+    private lateinit var binding: GameInitializerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_initializer, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.submitButton.setOnClickListener{ performAction() }
+    }
+
+    private fun setErrorTextFieldInitializer(error: Boolean) {
+        if (error) {
+            binding.textField.isErrorEnabled = true
+            binding.textField.error = getString(R.string.try_again)
+        } else {
+            binding.textField.isErrorEnabled = false
+            binding.textInputEditText.text = null
+        }
+    }
+
+    private fun performAction(){
+        val stringWeReceived=binding.textInputEditText.text.toString()
+        val maxNo=stringWeReceived.toDouble()
+        MAX_NO_OF_WORDS= maxNo.toInt()
+        val action=GameInitializerDirections.actionGameInitializerToGameFragment()
+        findNavController().navigate(action)
+    }
+}
